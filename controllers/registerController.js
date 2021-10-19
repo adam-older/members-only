2d14f9851ececfd06ea4f4f1854bd4ea8d8c29e5
Adam Older <adamolder10@gmail.com>
2021-10-18 19:22:46 -0700

    removed catch from username/email exist catch

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -14,30 +14,28 @@ exports.register_post = [
   body("username")
     .escape()
     .custom((value) => {
-      return User.findOne({ username: value })
-        .then((user) => {
-          if (user) {
-            return Promise.reject("Username is taken");
-          }
-        })
-        .catch((err) => {
-          return next(err);
-        });
+      return User.findOne({ username: value }).then((user) => {
+        if (user) {
+          return Promise.reject("Username is taken");
+        }
+      });
+      // .catch((err) => {
+      //   throw new Error("Server error");
+      // });
     }),
   body("email")
     .isEmail()
     .escape()
     .withMessage("Invalid Email")
     .custom((value) => {
-      return User.findOne({ email: value })
-        .then((user) => {
-          if (user) {
-            return Promise.reject("Email is already in use");
-          }
-        })
-        .catch((err) => {
-          return next(err);
-        });
+      return User.findOne({ email: value }).then((user) => {
+        if (user) {
+          return Promise.reject("Email is already in use");
+        }
+      });
+      // .catch((err) => {
+      //   throw new Error("Server error");
+      // });
     }),
   body("password")
     .escape()
@@ -64,12 +62,15 @@ exports.register_post = [
         errors: error_msgs,
       });
     } else {
-      // form data is good, register user
       var user = new User({
         username,
         email,
         password,
       });
+      // form data is good, register user
+      let salt = bcrypt.genSaltSync(10);
+      user.password = bcrypt.hashSync(user.password, salt);
+
       user.save((err) => {
         if (err) return next(err);
       });
